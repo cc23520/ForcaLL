61640de7ad62994dde5ba49a61a79bca1eebb885
GUILHERME DA ROCHA OLIVEIRA <u23520@academico.cotuca.unicamp.br>
2025-05-26 19:35:49 -0300

    a

---
+++ Projeto I - Forca/Listas Duplamente Ligadas/apForca/Forca.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Forca : IComparable<Forca>, IRegistro,
+					  ICriterioDeSeparacao<Forca>
+{
+	private Dica dica;
+	private Palavra palavra;
+
+	public Forca(Palavra palavra,Dica dica)
+	{
+		this.palavra = palavra;
+		this.dica = dica;
+	}
+
+	public Dica Dica { get => dica; set => dica = value; }
+	public Palavra Palavra { get => palavra; set => palavra = value; }
+
+    public int CompareTo(Forca other)
+    {
+        if (other == null) return 1;
+        return this.palavra.TextoPalavra.CompareTo(other.palavra.TextoPalavra);
+    }
+
+    public bool DeveSeparar()
+	{
+		throw new NotImplementedException();
+	}
+
+	public string FormatoDeArquivo()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string ToString()
+	{
+		return $"Palavra: {palavra.ToString()}, Dica: {dica.ToString()}";
+	}
+}
+
+
+
